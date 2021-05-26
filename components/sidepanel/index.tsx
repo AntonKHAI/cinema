9c706d6f1b0685e6b7c86524f5d3b658a11659d1
antonShepotko <anton.shepotko41@gmail.com>
2021-05-26 20:32:04 +0300

    minor update

---
+++ components/sidepanel/index.tsx
@@ -0,0 +1,10 @@
+import React,  { Component } from 'react'
+import styles from './SidePanel.module.css'
+
+class SidePanel extends Component {
+  render() {
+    return <div className={styles.main}>Side Panel</div>;
+  }
+}
+
+export default SidePanel
